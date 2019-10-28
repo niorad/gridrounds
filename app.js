7d53545a56aab1f0995844118d6cd1f4596c8f02
Antonio Radovcic <mail@niorad.com>
2019-10-28 17:27:17 +0100

    meh

--- app.js
+++ app.js
@@ -39,12 +39,51 @@ class App extends LitElement {
 				border: 1px dotted black;
 				display: inline-block;
 			}
+			main {
+				background: rgb(206, 208, 207);
+				max-width: 500px;
+				margin: auto;
+				margin-top: 2rem;
+				padding: 2px;
+				border-bottom-style: solid;
+				border-bottom-width: 2px;
+				border-image-outset: 0px;
+				border-image-repeat: stretch;
+				border-image-slice: 100%;
+				border-image-source: none;
+				border-image-width: 1;
+				border-left-color: rgb(255, 255, 255);
+				border-left-style: solid;
+				border-left-width: 2px;
+				border-right-color: rgb(5, 6, 8);
+				border-right-style: solid;
+				border-right-width: 2px;
+				border-top-color: rgb(255, 255, 255);
+				border-top-style: solid;
+				border-top-width: 2px;
+				box-shadow: rgb(223, 224, 227) 1px 1px 0px 1px inset,
+					rgb(136, 140, 143) -1px -1px 0px 1px inset;
+			}
+			h1 {
+				color: white;
+				font-size: 13px;
+				margin: 0 2px 0 0;
+				background-image: linear-gradient(
+					to right,
+					rgb(0, 0, 128),
+					rgb(16, 52, 166)
+				);
+				text-align: center;
+				padding: 5px;
+			}
+			.content {
+				padding: 1rem;
+			}
 			ul {
 				list-style: none outside none;
 				padding: 0;
 				display: grid;
 				grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-				grid-gap: 5px;
 				width: 100%;
 			}
 			li {
@@ -60,12 +99,13 @@ class App extends LitElement {
 			li button {
 				font-size: 3rem;
 				width: 100%;
+				margin: 0;
 			}
 			button {
-				font-family: 'Microsoft Sans Serif';
+				font-family: 'Microsoft Sans Serif', sans-serif;
 				font-size: 13px;
 				outline: 1px solid #000000;
-				background: #c0c0c0;
+				background: rgb(206, 208, 207);
 				border-width: 1px;
 				border-style: solid;
 				border-color: #ffffff #808080 #808080 #ffffff;
@@ -85,32 +125,37 @@ class App extends LitElement {
 	render() {
 		console.log(this.state);
 		return html`
-			Round: ${this.state.round} Lives: ${this.state.lives}
-			<ul>
-				${this.state.board.map((cell, index) => {
-					return html`
-						<li>
-							<button
-								@click=${e => {
-									this.onFieldClicked(index);
-								}}
-							>
-								${cell.occupant}
-							</button>
-							<small>${cell.timer}</small>
-						</li>
-					`;
-				})}
-			</ul>
-			<hr />
-			<button
-				class="fat-button"
-				@click=${() => {
-					this.onAdvanceRound({ type: actions.WAIT });
-				}}
-			>
-				Advance
-			</button>
+			<main>
+				<h1>GRIDROUNDS</h1>
+				<div class="content">
+					Round: ${this.state.round} Lives: ${this.state.lives}
+					<ul>
+						${this.state.board.map((cell, index) => {
+							return html`
+								<li>
+									<button
+										@click=${e => {
+											this.onFieldClicked(index);
+										}}
+									>
+										${cell.occupant}
+									</button>
+									<small>${cell.timer}</small>
+								</li>
+							`;
+						})}
+					</ul>
+					<hr />
+					<button
+						class="fat-button"
+						@click=${() => {
+							this.onAdvanceRound({ type: actions.WAIT });
+						}}
+					>
+						Advance
+					</button>
+				</div>
+			</main>
 		`;
 	}
 }
